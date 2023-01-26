2b1ae4b9d13a44448d25af833d0da89d7acafa3e
eltonnk <ahenri333@gmail.com>
2023-01-26 17:18:43 -0500

    Found state space representation of open-loop system with DO. Implemented but needs to be tested

--- simple.do.py
+++ simple.do.py
@@ -3,10 +3,17 @@ from typing import Callable
 import numpy as np
 import control
 
-class LinSysToIntegrate:
+class StateSpaceToIntegrate: # or, when shortened, ss2i
     nbr_states: int
     nbr_inputs: int
     nbr_outputs: int
+    ss : control.StateSpace
+
+    def __init__(self, ss: control.StateSpace):
+        self.ss = ss
+        self.nbr_states = ss.A.shape[0]
+        self.nbr_inputs = ss.B.shape[1]
+        self.nbr_outputs = ss.C.shape[0]
 
     def _check_valid_state(self, x: np.ndarray):
         if x.shape != (self.nbr_states, 1):
@@ -19,15 +26,6 @@ class LinSysToIntegrate:
     def _check_valid_output(self, y: np.ndarray):
         if y.shape != (self.nbr_outputs, 1):
             raise ValueError("Did not produce correct number of outputs.")
-
-class StateSpaceToIntegrate(LinSysToIntegrate): # or, when shortened, ss2i
-    ss : control.StateSpace
-
-    def __init__(self, ss: control.StateSpace):
-        self.ss = ss
-        self.nbr_states = ss.A.shape[0]
-        self.nbr_inputs = ss.B.shape[1]
-        self.nbr_outputs = ss.C.shape[0]
     
     def compute_state_derivative(self, x: np.ndarray, u: np.ndarray):
         self._check_valid_state(x)
@@ -45,35 +43,27 @@ class StateSpaceToIntegrate(LinSysToIntegrate): # or, when shortened, ss2i
         self._check_valid_output(y)
         return y
 
-class LinSysToIntegrateGenerator(LinSysToIntegrate):
-    nbr_states: int
-    nbr_inputs: int
-    nbr_outputs: int
-    def __init__(
-        self, 
-        list_state_space_to_integrate: list[StateSpaceToIntegrate], 
-        nbr_inputs: int, 
-        nbr_outputs: int
-    ):
-        self.nbr_states = sum([ss2i.nbr_states for ss2i in list_state_space_to_integrate])
-        self.nbr_inputs = nbr_inputs
-        self.nbr_outputs = nbr_outputs
-
-    def compute_state_derivative_generator(
-        self, 
-        compute_state_derivative_x_u: Callable[[np.ndarray, np.ndarray], np.ndarray],
+    def generate_fction_to_integrate(
+        self,
         compute_u_from_t: Callable[[float], np.ndarray],
     ):
 
-        def compute_state_derivative(t: float, x: np.ndarray):
+        def fction_to_integrate(t: float, x: np.ndarray):
             u = compute_u_from_t(t)
-            self._check_valid_input(u)
-            self._check_valid_state(x)
-            x_dot = compute_state_derivative_x_u(x, u)
-            self._check_valid_state(x_dot)
+            x_dot = self.compute_state_derivative(x, u)
             return x_dot
 
-        return compute_state_derivative
+        return fction_to_integrate
+
+    def recreate_output(
+        self, 
+        compute_u_from_t: Callable[[float], np.ndarray],
+        t_arr: np.ndarray, 
+        x_arr: np.ndarray,
+    ):
+    # Have to use np.vectorize with signature argument somewhat
+        pass
+    
 
 
 
@@ -81,51 +71,84 @@ if __name__ == '__main__':
     k = 2.  # kg
     c = .2  # Ns/m
     m = 0.1 # kg
-    A_sys = np.array([[0.,1.],
+    A_n = np.array([[0.,1.],
                       [-k/m, -c/m]])
-    B_sys = np.array([0.,
+    B_n = np.array([0.,
                      1/m])
 
-    C_sys = np.array([[1.0, 0.0]])
-    D_sys = np.array([[0.]])
+    C_n = np.array([[1.0, 0.0]])
+    D_n = np.array([[0.]])
 
     low_pass_cutoff_frequency = 500
+    low_pass_damping_coeff = 0.707
 
-    ss_sys = control.StateSpace(A_sys, B_sys, C_sys, D_sys)
+    ss_n = control.StateSpace(A_n, B_n, C_n, D_n)
 
-    P_sys = control.ss2tf(ss_sys) # this is the transfer fucntion from input force to ouput position
+    P_n = control.ss2tf(ss_n) # this is the transfer fucntion from input force to ouput position
 
     s = control.tf('s')
     
-    G = 1 /(s + 500)
+    Q = low_pass_cutoff_frequency**2 /(s**2 + 2*low_pass_damping_coeff*low_pass_cutoff_frequency*s + low_pass_cutoff_frequency**2)
 
-    G_P_inverse = control.minreal(G * P_sys^-1)
+    Q_P_inverse = control.minreal(Q / P_n)
 
-    ss_G = control.tf2ss(G)
+    ss_Q = control.tf2ss(Q)
 
-    ss_G_P_inverse = control.tf2ss(G_P_inverse)
+    ss_Q_P_inverse = control.tf2ss(Q_P_inverse)
 
-    ss2i_P = StateSpaceToIntegrate(P_sys)
-    ss2i_G = StateSpaceToIntegrate(G)
-    ss2i_G_P_inverse = StateSpaceToIntegrate(G_P_inverse)
+    A_q = ss_Q.A
+    B_q = ss_Q.B
+    C_q = ss_Q.C
+    D_q = ss_Q.D
 
-    all_ss2i = [ss2i_P, ss2i_G, ss2i_G_P_inverse]
+    A_c = ss_Q_P_inverse.A
+    B_c = ss_Q_P_inverse.B
+    C_c = ss_Q_P_inverse.C
+    D_c = ss_Q_P_inverse.D
 
-    fct_for_scipy_integrate_gen = LinSysToIntegrateGenerator(all_ss2i, 2, 3)
+    M_inv = np.linalg.inv(np.eye(D_q.shape(0)) - D_q + D_c @ D_n)
+
+    M_inv_D_c_C_n = (M_inv @ D_c @ C_n)
+    M_inv_C_q = (M_inv @ C_q)
+    M_inc_C_c = (M_inv @ C_c)
+
+    C_n_m_D_n_M_inv_D_c_C_n = (C_n - D_n @ M_inv_D_c_C_n)
+    D_n_M_inv_C_q = (D_n @ M_inv_C_q)
+    D_n_M_inc_C_c = (D_n @ M_inc_C_c)
+
+    A_tot = np.block([
+        [A_n - B_n @ M_inv_D_c_C_n,     B_n @ M_inv_C_q,       -B_n @ M_inc_C_c],
+        [-B_q @ M_inv_D_c_C_n,          A_q + B_q @ M_inv_C_q, -B_q @ M_inc_C_c],
+        [B_c @ C_n_m_D_n_M_inv_D_c_C_n, B_c @ D_n_M_inv_C_q ,  -B_c @ D_n_M_inc_C_c],
+    ])
+
+    M_inv_D_c_Dn = (M_inv @ D_c @ D_n)
+    I_m_M_inv_D_c_Dn = np.eye(M_inv_D_c_Dn.shape(0)) - M_inv_D_c_Dn
+    M_inv_D_q = M_inv @ D_q
+    I_m_M_inv__D_c_Dn_m_Dq_ = I_m_M_inv_D_c_Dn + M_inv_D_q
+    D_n_I_m_M_inv__D_c_Dn_m_Dq_ = D_n @ I_m_M_inv__D_c_Dn_m_Dq_
+    D_n_I_m_M_inv_D_c_Dn = D_n @ I_m_M_inv_D_c_Dn
+
+    B_tot = np.block([
+        [B_n @ I_m_M_inv__D_c_Dn_m_Dq_ ,     B_n @ I_m_M_inv_D_c_Dn    ],
+        [B_q @ I_m_M_inv__D_c_Dn_m_Dq_ ,     -B_q @ M_inv_D_c_Dn       ],
+        [B_c @  D_n_I_m_M_inv__D_c_Dn_m_Dq_, B_c @ D_n_I_m_M_inv_D_c_Dn],
+    ])
+
+    C_tot = np.block([
+        [C_n_m_D_n_M_inv_D_c_C_n, D_n_M_inv_C_q, -D_n_M_inc_C_c],
+        [M_inv_D_c_C_n,           -M_inv_C_q,    M_inc_C_c     ],
+    ])
+
+    D_tot = np.block([
+        [D_n_I_m_M_inv__D_c_Dn_m_Dq_, D_n_I_m_M_inv_D_c_Dn],
+        [M_inv_D_c_Dn - M_inv_D_q,    M_inv_D_c_Dn],
+    ])
 
 
-    def csd_xu_open_loop_sys_with_do(x: np.ndarray, u:np.ndarray):
+    ss_tot = control.StateSpace(A_tot, B_tot, C_tot, D_tot)
 
-        P_sys_x = x[0:ss2i_P.nbr_states, :]
-        G_x = x[ss2i_P.nbr_states: ss2i_G.nbr_states, :]
-        G_P_inverse_x = x[ss2i_G.nbr_states: ss_G_P_inverse.nbr_states, :]
-
-        u_in = u[0, :]
-        d = u[1, :]
-
-        
-        u_estim 
-
+    ss2i_tot = StateSpaceToIntegrate(ss_tot)
 
 
 
