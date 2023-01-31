1ee4887cac1e1204772b6782d9aa36c5c505c97a
eltonnk <ahenri333@gmail.com>
2023-01-31 17:15:16 -0500

    not working but on its way

--- simple.do.py
+++ simple.do.py
@@ -2,6 +2,8 @@ from typing import Callable
 
 import numpy as np
 import control
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
 
 class StateSpaceToIntegrate: # or, when shortened, ss2i
     nbr_states: int
@@ -15,12 +17,13 @@ class StateSpaceToIntegrate: # or, when shortened, ss2i
         self.nbr_inputs = ss.B.shape[1]
         self.nbr_outputs = ss.C.shape[0]
 
-    def _check_valid_state(self, x: np.ndarray):
-        if x.shape != (self.nbr_states, 1):
+    def _check_valid_state(self, x: np.ndarray) -> np.ndarray:
+        if x.shape[0] != self.nbr_states:
             raise ValueError("Did not provide correct number of states.")
+        return x.reshape((self.nbr_states, 1))
 
-    def _check_valid_input(self, u: np.ndarray):
-         if u.shape != (self.nbr_inputs, 1):
+    def _check_valid_input(self, u: np.ndarray) :
+        if u.shape != (self.nbr_inputs, 1):
             raise ValueError("Did not provide correct number of inputs.")
             
     def _check_valid_output(self, y: np.ndarray):
@@ -55,15 +58,21 @@ class StateSpaceToIntegrate: # or, when shortened, ss2i
 
         return fction_to_integrate
 
-    def recreate_output(
+    def recreate_input_and_output(
         self, 
         compute_u_from_t: Callable[[float], np.ndarray],
         t_arr: np.ndarray, 
         x_arr: np.ndarray,
     ):
-    # Have to use np.vectorize with signature argument somewhat
-        pass
-    
+        u_arr = np.zeros(self.nbr_inputs, t_arr.shape[0])
+        y_arr = np.zeros(self.nbr_outputs, t_arr.shape[0])
+
+        for i, t in enumerate(t_arr):
+            u = compute_u_from_t(t)
+            u_arr[i, :] = u
+            y_arr[i, :] = self.compute_output(x_arr[i,:], u)
+
+        return u_arr, y_arr
 
 
 
@@ -71,13 +80,21 @@ if __name__ == '__main__':
     k = 2.  # kg
     c = .2  # Ns/m
     m = 0.1 # kg
-    A_n = np.array([[0.,1.],
-                      [-k/m, -c/m]])
-    B_n = np.array([0.,
-                     1/m])
+    A_n = np.array([
+        [0.,1.],
+        [-k/m, -c/m]
+    ])
+    B_n = np.array([
+        [0.],
+        [1/m]
+    ])
 
-    C_n = np.array([[1.0, 0.0]])
-    D_n = np.array([[0.]])
+    C_n = np.array([
+        [1.0, 0.0]
+    ])
+    D_n = np.array([
+        [0.]
+    ])
 
     low_pass_cutoff_frequency = 500
     low_pass_damping_coeff = 0.707
@@ -106,7 +123,7 @@ if __name__ == '__main__':
     C_c = ss_Q_P_inverse.C
     D_c = ss_Q_P_inverse.D
 
-    M_inv = np.linalg.inv(np.eye(D_q.shape(0)) - D_q + D_c @ D_n)
+    M_inv = np.linalg.inv(np.eye(D_q.shape[0]) - D_q + D_c @ D_n)
 
     M_inv_D_c_C_n = (M_inv @ D_c @ C_n)
     M_inv_C_q = (M_inv @ C_q)
@@ -123,7 +140,7 @@ if __name__ == '__main__':
     ])
 
     M_inv_D_c_Dn = (M_inv @ D_c @ D_n)
-    I_m_M_inv_D_c_Dn = np.eye(M_inv_D_c_Dn.shape(0)) - M_inv_D_c_Dn
+    I_m_M_inv_D_c_Dn = np.eye(M_inv_D_c_Dn.shape[0]) - M_inv_D_c_Dn
     M_inv_D_q = M_inv @ D_q
     I_m_M_inv__D_c_Dn_m_Dq_ = I_m_M_inv_D_c_Dn + M_inv_D_q
     D_n_I_m_M_inv__D_c_Dn_m_Dq_ = D_n @ I_m_M_inv__D_c_Dn_m_Dq_
@@ -149,6 +166,70 @@ if __name__ == '__main__':
     ss_tot = control.StateSpace(A_tot, B_tot, C_tot, D_tot)
 
     ss2i_tot = StateSpaceToIntegrate(ss_tot)
+
+
+    def compute_input_and_disturb_from_t(t: float) -> np.ndarray:
+        # input is a step
+        am_u_in = 5
+        if t > 0.01:
+            u_in = am_u_in # N
+        else:
+            u_in = 0 # N
+        
+        amp_d = 0.5 
+        d = amp_d * np.sin(t)
+
+        return np.array([
+            [u_in],
+            [d]
+        ])
+
+    fction_to_integrate = ss2i_tot.generate_fction_to_integrate(compute_input_and_disturb_from_t)
+
+    x0 = np.zeros((A_tot.shape[0], 1)).ravel()
+
+
+    dt = 1e-3
+    t_start = 0
+    t_end = 5
+    t = np.arange(t_start, t_end, dt)
+    # Find time-domain response by integrating the ODE
+    sol = integrate.solve_ivp(
+        fction_to_integrate,
+        (t_start, t_end),
+        x0,
+        t_eval=t,
+        rtol=1e-6,
+        atol=1e-6,
+        method='RK45',
+    )
+
+    sol_x = sol.y
+    sol_t = sol.t
+
+    sol_u, sol_y = ss2i_tot.recreate_input_and_output(compute_input_and_disturb_from_t, sol_t, sol_x)
+
+    fig, ax = plt.subplots(2,1)
+
+    ax[0].set_xlabel(r'$t$ (s)')
+    ax[0].set_ylabel(r'$q(t)$ (N)')
+    # Plot data
+
+    ax[0].plot(sol_t, sol_y[0,:], label=r'$q(t)$', color='C0')
+    ax[0].plot(sol_t, sol_u[0,:], label=r'$u_{in}(t)$', color='C1')
+    ax[0].legend(loc='upper right')
+
+    ax[1].set_xlabel(r'$t$ (s)')
+    ax[1].set_ylabel(r'$x(t)$ (rad and rad/s)')
+    # Plot data
+
+    ax[0].plot(sol_t, sol_y[1,:], label=r'$\tilde{d}(t)$', color='C0')
+    ax[0].plot(sol_t, sol_u[1,:], label=r'$d(t)$', color='C1')
+    ax[1].legend(loc='upper right')
+
+    fig.tight_layout()
+    plt.show()
+
 
 
 
